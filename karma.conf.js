63db40f46ddb01fe528a02836e69b1c41f6b403a
unknown <financeirinhoaplicativo@gmail.com>
2021-12-14 00:33:40 -0300

    fix code

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/component-three-omar'),
+      dir: require('path').join(__dirname, './coverage/financeirinhoapp'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
